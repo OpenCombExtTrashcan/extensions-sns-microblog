e9160322b869a9fb0bd411c51a0a9228ce64dbba
alee <alee@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-27 05:54:14 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@44 34e84b91-b3e8-45e1-a0bc-4720add8a18f

---
+++ class/MicroBlog.php
@@ -0,0 +1,19 @@
+<?php
+namespace oc\ext\microblog ;
+
+use jc\mvc\model\db\orm\ModelAssociationMap;
+
+use jc\db\DB ;
+use jc\db\PDODriver ;
+
+use oc\ext\Extension;
+
+class MicroBlog extends Extension
+{
+	public function load()
+	{
+	}
+	
+}
+
+?>
