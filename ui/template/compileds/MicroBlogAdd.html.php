a61d2b80ce68a9d19b61d5331d806d71e6a0cd6b
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-29 06:21:30 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@52 34e84b91-b3e8-45e1-a0bc-4720add8a18f

---
+++ ui/template/compileds/MicroBlogAdd.html.php
@@ -0,0 +1,25 @@
+
+<?php 
+$__ui_msgqueue = eval("if(!isset(\$__uivar_theView)){ \$__uivar_theView=&\$aVariables->getRef('theView') ;};
+return \$__uivar_theView;") ;
+if( $__ui_msgqueue instanceof \jc\message\IMessageQueueHolder )
+{ $__ui_msgqueue = $__ui_msgqueue->messageQueue() ; }
+\jc\lang\Assert::type( '\\jc\\message\\IMessageQueue',$__ui_msgqueue);
+if( $__ui_msgqueue->count() ){ 
+	$__ui_msgqueue->display($this,$aDevice) ;
+} ?>
+
+<?php if( !($aVariables->get('theView') instanceof \jc\mvc\view\FormView) or $aVariables->get('theView')->isShowForm() ) { ?>
+<form action="/?c=MicroBlogAdd" method="post">
+    <div>       
+		<?php $_aWidget = $aVariables->get('theView')->widget("text") ;
+if($_aWidget){
+	$_aWidget->display($this,null,$aDevice) ;
+}else{
+	echo '缺少 widget (id:'."text".')' ;
+} ?>
+       
+	</div>
+    
+    <input type="submit" value="发布" />
+<input type="hidden" name="<?php echo $aVariables->get('theView')->htmlFormSignature()?>" value="1" /></form><?php } ?>
