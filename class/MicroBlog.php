8b42e88b18b6eeec3b20c5eddf4b2daa8e42da02
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-29 04:21:44 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@50 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/MicroBlog.php
+++ class/MicroBlog.php
@@ -1,19 +1,102 @@
 <?php
-namespace oc\ext\microblog ;
 
-use jc\mvc\model\db\orm\ModelAssociationMap;
+// +----------------------------------------------------------------------
+// | WeiBo 
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011 http://www.sunmy.com.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.X )
+// +----------------------------------------------------------------------
+// | Author: luwei <solver.lu@gmail.com>
+// +----------------------------------------------------------------------
+// 1.0.0.1 
 
-use jc\db\DB ;
-use jc\db\PDODriver ;
+namespace oc\ext\microblog;
 
-use oc\ext\Extension;
+//调用共通类
+use jc\mvc\model\db\orm\ModelAssociationMap;    //
+use jc\db\DB;                                   //
+use jc\db\PDODriver;                            //
+use oc\ext\Extension;                           //
 
-class MicroBlog extends Extension
-{
-	public function load()
-	{
-	}
-	
+/**
+ *   微博配置类
+ *   @package    microblog
+ *   @author     luwei
+ *   @created    2011-06-28
+ *   @history     
+ */
+class MicroBlog extends Extension {
+
+    /**
+     *    加载方法
+     *    @param      null
+     *    @package    microblog 
+     *    @return     null
+     *    @author     luwei
+     *    @created    2011-06-28
+     */
+    public function load() {
+        //模型关系实例
+        $aAssocMap = ModelAssociationMap::singleton();
+        //microblog模型关系
+        $aAssocMap->addOrm(
+                array(
+                    'keys' => 'mbid', //主键
+                    'table' => 'microblog', //模型名称
+                    //与topic关系
+                    'hasOne' => array(
+                        array(
+                            'prop' => 'topic', //属性名
+                            'fromk' => 'mbid', //主键
+                            'tok' => 'mbid', //外键
+                            'model' => 'topic'  //模型名称
+                        ),
+                    ),
+                    //与user关系
+                    'belongsTo' => array(
+                        array(
+                            'prop' => 'user', //属性名
+                            'fromk' => 'uid', //主键
+                            'tok' => 'uid', //外键
+                            'model' => 'user' //模型名称
+                        )
+                    ),
+                    //与at关系
+                    'hasAndBelongsToMany' => array(
+                        array(
+                            'prop' => 'at', //属性名
+                            'fromk' => 'mbid', //主键
+                            'tok' => 'mbid', //外键
+                            'bfromk' => 'uid', //从主键
+                            'btok' => 'at_uid', //从外键
+                            'bridge' => 'at', //从模型名称
+                            'model' => 'microblog', //模型名称
+                        ),
+                    ),
+                )
+        );
+        //topic模型关系
+        $aAssocMap->addOrm(
+                array(
+                    'keys' => 'mbid', //主键
+                    'table' => 'topic', //模型名称
+                    //与microblog关系
+                    'belongsTo' => array(
+                        array(
+                            'prop' => 'microblog', //属性名
+                            'fromk' => 'mbid', //主键
+                            'tok' => 'mbid', //外键
+                            'model' => 'microblog' //模型名称
+                        )
+                    ),
+                )
+        );
+
+        //加载微博列表控制器
+        $this->application()->accessRouter()->addController('MicroBlogAdd', "oc\\ext\\microblog\\MicroBlogAdd");
+    }
+
 }
 
 ?>
---
+++ class/MicroBlogAdd.php
@@ -0,0 +1,117 @@
+<?php
+
+// +----------------------------------------------------------------------
+// | WeiBo 
+// +----------------------------------------------------------------------
+// | Copyright (c) 2011 http://www.sunmy.com.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.X )
+// +----------------------------------------------------------------------
+// | Author: luwei <solver.lu@gmail.com>
+// +----------------------------------------------------------------------
+// 1.0.0.1
+
+namespace oc\ext\microblog;
+
+//调用共通类
+use jc\mvc\controller\Controller;               //控制器类
+use oc\base\FrontFrame;                         //视图框架类
+use jc\mvc\model\db\Model;                      //模型类
+use jc\mvc\view\widget\Text;                    //文本组件类
+use jc\mvc\model\db\orm\ModelAssociationMap;    //模型关系类
+use jc\message\Message;                        //消息类
+use jc\verifier\Length;                         //长度校验类
+use jc\verifier\NotEmpty;                       //非空校验类
+use jc\mvc\view\DataExchanger ;                 //数据交换类
+/**
+ *   微博发布类
+ *   @package    microblog
+ *   @author     luwei
+ *   @created    2011-06-28
+ *   @history     
+ */
+
+class MicroBlogAdd extends Controller {
+
+    /**
+     *    初始化方法
+     *    @param      null
+     *    @package    microblog 
+     *    @return     null
+     *    @author     luwei
+     *    @created    2011-06-28
+     */
+    protected function init() {
+        // 加载视图框架
+        $this->add(new FrontFrame());
+
+        //创建默认视图
+        $this->createView("defaultView", "MicroBlogAdd.html", true);
+
+        //为视图创建、添加textarea文本组件
+        $this->defaultView->addWidget(new Text("text", "内容", "", Text::multiple), 'text')
+                ->addVerifier(Length::flyweight(1, 140));
+        $this->defaultView->addWidget(new Text("text", "内容", "", Text::multiple), 'text')
+                ->addVerifier(NotEmpty::singleton(), "必须输入");
+
+        /*
+          // 为视图创建、添加images文本组件
+          $this->defaultView->addWidget( new Text("images","图片"), 'images' );
+
+          // 为视图创建、添加videos文本组件
+          $this->defaultView->addWidget( new Text("videos","图片"), 'videos' );
+
+          // 为视图创建、添加musics文本组件
+          $this->defaultView->addWidget( new Text("musics","图片"), 'musics' );
+         * 
+         */
+
+        //设定模型
+        $this->defaultView->setModel(Model::fromFragment('microblog'));
+    }
+
+    /**
+     *    业务逻辑处理
+     *    @param      null
+     *    @package    weibo 
+     *    @return     null
+     *    @author     luwei
+     *    @created    2011-06-28
+     */
+    public function process() {
+        //判断表单是否提交
+        if ($this->defaultView->isSubmit($this->aParams)) {
+            
+            // 加载 视图组件的数据
+            $this->defaultView->loadWidgets($this->aParams);
+            
+            // 校验 视图组件的数据
+            if ($this->defaultView->verifyWidgets()) {
+                
+                //将视图组件的数据与模型交换
+                $this->defaultView->exchangeData(DataExchanger::WIDGET_TO_MODEL) ;
+                
+                //用户ID
+                $this->defaultView->model()->setData('uid', '1');
+
+                //发布时间
+                $this->defaultView->model()->setData('time', time());
+
+                //客户端
+                $this->defaultView->model()->setData('client', 'web');
+
+                try {
+                    $this->defaultView->model()->save();
+                    $this->defaultView->createMessage(Message::success, "发布成功！");
+
+                    $this->defaultView->hideForm();
+                } catch (ExecuteException $e) {
+                    throw $e;
+                }
+            }
+        }
+    }
+
+}
+
+?>
