8dec77789d99d4de697724479a0cafc763831558
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-06-29 09:46:29 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@61 34e84b91-b3e8-45e1-a0bc-4720add8a18f

---
+++ ui/template/compileds/MicroBlogList.html.php
@@ -0,0 +1,17 @@
+<?php
+				$__foreach_Arr_var0 = eval("if(!isset(\$__uivar_theModel)){ \$__uivar_theModel=&\$aVariables->getRef('theModel') ;};
+return \$__uivar_theModel->childIterator();");
+				if(!empty($__foreach_Arr_var0)){ 
+					$__foreach_idx_var3 = -1;
+					foreach($__foreach_Arr_var0 as $__foreach_key_var2 => &$__foreach_item_var1){
+						$__foreach_idx_var3++;
+						 $aVariables->set("row",$__foreach_item_var1 ); ?>
+	
+			<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('text');") ;?>  <a href="?cMicroBlogDelete&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('mbid');") ;?>">删除</a> <br />
+			
+<?php 
+					}
+				}
+			 		?>
