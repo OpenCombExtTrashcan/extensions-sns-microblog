d68e30e030e656e555e6a18b19d717432f50b07b
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-01 03:05:36 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@80 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- class/MicroBlogList.php
+++ class/MicroBlogList.php
@@ -1,4 +1,5 @@
 <?php
+
 // +----------------------------------------------------------------------
 // | WeiBo 
 // +----------------------------------------------------------------------
@@ -26,8 +27,9 @@ use jc\auth\IdManager;                          //用户SESSION类
  *   @created    2011-06-29
  *   @history     
  */
+
 class MicroBlogList extends Controller {
-    
+
     /**
      *    初始化方法
      *    @param      null
@@ -37,18 +39,17 @@ class MicroBlogList extends Controller {
      *    @created    2011-06-29
      */
     protected function init() {
-        
+
         // 加载视图框架
         $this->add(new FrontFrame());
-        
+
         //创建默认视图
         $this->createView("defaultView", "MicroBlogList.html", true);
-                
+
         //设定模型
-        $this->defaultView->setModel(Model::fromFragment('microblog',array(),true));
-        
+        $this->defaultView->setModel(Model::fromFragment('microblog', array(), true));
     }
-    
+
     /**
      *    业务逻辑处理
      *    @param      null
@@ -59,10 +60,43 @@ class MicroBlogList extends Controller {
      */
     public function process() {
         
+        /*
+          微博中过滤话题和对象名的实例
+          =============================================================================================================================
+          $post_content = "@oschina和@twitter在研究用#PHP#的#正则表达式#过滤话题和对象名";
+
+          $tag_pattern = "/\#([^\#|.]+)\#/";
+          preg_match_all($tag_pattern, $post_content, $tagsarr);
+          var_dump($tagsarr);
+          $tags = implode(',', $tagsarr[1]);
+          echo $tags;
+          $user_pattern = "/\@([a-zA-z0-9_]+)/";
+          $post_content = preg_replace($user_pattern, '<a href="http://twitter.com/${1}">@${1}</a>', $post_content);
+          $post_content = preg_replace($tag_pattern, '<a href="http://twitter.com/search?q=#${1}">#${1}#</a>', $post_content);
+
+          echo $post_content;
+          ===============================================================================================================================
+         */
+
+        //过滤话题的正则表达式
+        $tag_pattern = "/\#([^\#|.]+)\#/";
+
+        //过滤@用户的正则表达式
+        $user_pattern = "/\@([a-zA-z0-9_]+)/";
+
         //载入当前用户的所有微博
         $userList = IdManager::fromSession();
-        $this->defaultView->model()->load($userList->currentId()->userId(),"uid");
+        $this->defaultView->model()->load($userList->currentId()->userId(), "uid");   
+        
+        //过滤话题和对象名
+        foreach ($this->defaultView->model()->childIterator() as $row){
+            $text = $row->data("text");
+            $text = preg_replace($user_pattern, '<a href=/${1}>@${1}</a>', $text); 
+            $text = preg_replace($tag_pattern, '<a href="/k/${1}">#${1}#</a>', $text);
+            $row->setData("text",$text);
+		}
     }
+
 }
 
 ?>
