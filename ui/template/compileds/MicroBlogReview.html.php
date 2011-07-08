c9ffcafedabbb1fb9f922625c3909d373aa12522
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-08 05:59:57 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@165 34e84b91-b3e8-45e1-a0bc-4720add8a18f

---
+++ ui/template/compileds/MicroBlogReview.html.php
@@ -0,0 +1,19 @@
+
+<?php
+				$__foreach_Arr_var0 = eval("if(!isset(\$__uivar_theModel)){ \$__uivar_theModel=&\$aVariables->getRef('theModel') ;};
+return \$__uivar_theModel->childIterator();");
+				if(!empty($__foreach_Arr_var0)){ 
+					$__foreach_idx_var3 = -1;
+					foreach($__foreach_Arr_var0 as $__foreach_key_var2 => &$__foreach_item_var1){
+						$__foreach_idx_var3++;
+						 $aVariables->set("row",$__foreach_item_var1 );  $aVariables->set("idx",$__foreach_idx_var3 ); ?>					
+			<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->child('user')->data('username');") ;?>:<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('text');") ;?>  <a href="?c=microblog.MicroBlogReview&id=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('mbid');") ;?>&rid=<?php echo eval("if(!isset(\$__uivar_row)){ \$__uivar_row=&\$aVariables->getRef('row') ;};
+return \$__uivar_row->data('rid');") ;?>">回复</a><br />
+<?php 
+					}
+				}
+			 		?>
+
