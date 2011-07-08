2642b576b5fcc6888d9b63d7543c7ace927ed84f
luwei <luwei@34e84b91-b3e8-45e1-a0bc-4720add8a18f>
2011-07-08 06:06:52 +0000

    git-svn-id: http://svn.opencomb.com/oc/extensions/sns/microblog/trunk@166 34e84b91-b3e8-45e1-a0bc-4720add8a18f

--- ui/template/compileds/moodlist.html.php
+++ ui/template/compileds/moodlist.html.php
@@ -5,7 +5,7 @@ return count(\$__uivar_myMood)>0;")){ ?>
 return (\$__uivar_myMood['type']=='1')?\"好\":((\$__uivar_myMood['type']=='2')?\"坏\":\"一般\");") ;?> <br />
 &nbsp;&nbsp;我:<?php echo eval("if(!isset(\$__uivar_myMood)){ \$__uivar_myMood=&\$aVariables->getRef('myMood') ;};
 return \$__uivar_myMood['text'];") ;?> <br />
-
+相同心情的朋友：<br />
 <?php
 				$__foreach_Arr_var0 = eval("if(!isset(\$__uivar_theModel)){ \$__uivar_theModel=&\$aVariables->getRef('theModel') ;};
 return \$__uivar_theModel->childIterator();");
